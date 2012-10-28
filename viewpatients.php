e46150f7939a9ed90a6ba53d3e95af33192ed26d
yogi.hammerhead@gmail.com <yogi.hammerhead@gmail.com@031417a3-609b-2138-e9d1-44dea2c5b485>
2012-10-28 15:46:26 +0000

    PHP script to extract values from the patients database.

---
+++ viewpatients.php
@@ -0,0 +1,31 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = 'yogi123';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+$sql = 'SELECT * from patients';
+
+mysql_select_db('hms');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not get data: ' . mysql_error());
+}
+while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+{
+    echo "PATIENT ID :{$row['id']}  <br> ".
+         "NAME 		 : {$row['name']} <br> ".
+         "AGE		 : {$row['age']} <br> ".
+         "GENDER	 : {$row['gender']} <br> ".
+         "OCCUPATION : {$row['occupation']} <br> ".
+         "MOBILE	 : {$row['mobile']} <br> ".
+         "ADDRESS	 : {$row['address']} <br> ".
+         "--------------------------------<br>";
+} 
+
+mysql_close($conn);
+?>
